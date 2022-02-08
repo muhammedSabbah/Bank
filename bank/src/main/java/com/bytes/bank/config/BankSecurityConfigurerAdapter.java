b23805ad31ca5c7afe88c801378413feb8ed7236
unknown <mohamedsabbah198@gmail.com>
2022-02-08 16:46:24 +0200

    inMemory Authentication user,password, and authorities

--- bank/src/main/java/com/bytes/bank/config/BankSecurityConfigurerAdapter.java
+++ bank/src/main/java/com/bytes/bank/config/BankSecurityConfigurerAdapter.java
@@ -2,8 +2,10 @@ package com.bytes.bank.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 public class BankSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
@@ -20,6 +22,19 @@ public class BankSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter 
 	@Value("${app.url.base.myCards}")
 	private String CARDS;
 	
+	
+	
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("bank").password("123456").authorities("admin")
+		.and().withUser("mohamed").password("456").authorities("read")
+		.and().passwordEncoder(NoOpPasswordEncoder.getInstance());
+	}
+
+
+
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
