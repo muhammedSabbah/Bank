e72b05a0b9ca7d05067cdab54367f07035713d86
unknown <mohamedsabbah198@gmail.com>
2022-02-10 17:39:28 +0200

    auth

--- bank/src/main/java/com/bytes/bank/config/UserDetailsServiceImpl.java
+++ bank/src/main/java/com/bytes/bank/config/UserDetailsServiceImpl.java
@@ -1,37 +1,24 @@
 package com.bytes.bank.config;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserDetailsServiceImpl implements UserDetailsService {
+public class UserDetailsServiceImpl extends InMemoryUserDetailsManager {
 	
-	static List<UserDetails> userDetails;
-	static {
-		userDetails = new ArrayList<UserDetails>();
-	}
-
-	{
-		userDetails.add(User.withUsername("bank").password("123456").authorities("admin").build());
-		userDetails.add(User.withUsername("mohamed").password("123").authorities("read").build());
+	public UserDetailsServiceImpl() {
+		createUser(User.withUsername("bank").password("123456").authorities("admin").build());
+		createUser(User.withUsername("mohamed").password("123").authorities("read").build());
 	}
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		System.out.println("SABBAH - UserDetailsServiceImpl - loadUserByUsername");
 		System.out.println("user : " + username);
-		for(UserDetails user : userDetails) {
-			if(user.getUsername().equals(username)) {
-				return user;
-			}
-		}
-		return null;
+		return super.loadUserByUsername(username);
 	}
 
 }
