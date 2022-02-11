b79055265d3bfe481de0fca07aecf63f33a5cfbb
unknown <mohamedsabbah198@gmail.com>
2022-02-11 14:45:47 +0200

    base component

--- bank/src/main/java/com/bytes/bank/controller/AccountController.java
+++ bank/src/main/java/com/bytes/bank/controller/AccountController.java
@@ -3,11 +3,14 @@ package com.bytes.bank.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.module.manager.basecomponent.BaseComponent;
+
 @RestController
-public class AccountController {
+public class AccountController extends BaseComponent{
 
 	@GetMapping("${app.url.base.myAccount}")
 	public String getMyAccount() {
+		getLogger().info("MY ACCOUNT");
 		return "MY ACCOUNT";
 	}
 	
