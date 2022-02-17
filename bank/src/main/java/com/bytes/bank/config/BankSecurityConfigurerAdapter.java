7d056ebfa1d1a11f9664f83107480b178b610f5b
unknown <mohamedsabbah198@gmail.com>
2022-02-18 01:25:45 +0200

    clear

--- bank/src/main/java/com/bytes/bank/config/BankSecurityConfigurerAdapter.java
+++ bank/src/main/java/com/bytes/bank/config/BankSecurityConfigurerAdapter.java
@@ -7,12 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class BankSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
