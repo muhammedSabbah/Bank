5e1c925298bf8fd24295ac64e1cb189176b52bdf
unknown <mohamedsabbah198@gmail.com>
2022-02-18 01:35:03 +0200

    default password encoder

--- bank/src/main/java/com/bytes/bank/config/BankSecurityConfigurerAdapter.java
+++ bank/src/main/java/com/bytes/bank/config/BankSecurityConfigurerAdapter.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
