3f519edeae4f7c578a0f0d58d86bfd5c8a852c60
unknown <mohamedsabbah198@gmail.com>
2022-03-01 19:13:10 +0200

    add basic authentication filter before

--- bank/src/main/java/com/bytes/bank/config/BankSecurityConfigurerAdapter.java
+++ bank/src/main/java/com/bytes/bank/config/BankSecurityConfigurerAdapter.java
@@ -4,23 +4,22 @@ import java.util.Collections;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
+@EnableWebSecurity(debug = true)
 public class BankSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
 
 
@@ -98,6 +97,7 @@ public class BankSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter 
 		})
 		.and().csrf().ignoringAntMatchers(NEW_ACCOUNT).csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
 		.and()
+		.addFilterBefore(new RequestValidationBeforeFilter(), BasicAuthenticationFilter.class)
 		.authorizeHttpRequests()
 		.antMatchers(HttpMethod.GET, ACCOUNT).hasAuthority("USER")
 		.antMatchers(BALANCE).authenticated()
---
+++ bank/src/main/java/com/bytes/bank/config/RequestValidationBeforeFilter.java
@@ -0,0 +1,52 @@
+package com.bytes.bank.config;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.util.StringUtils;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+public class RequestValidationBeforeFilter implements Filter{
+
+	private static final String AUTHENTICATION_SCHEME_BASIC = "Basic";
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+		String header = req.getHeader(AUTHORIZATION);
+		if(header != null) {
+			header = header.trim();
+			if(StringUtils.startsWithIgnoreCase(header, AUTHENTICATION_SCHEME_BASIC)) {
+				byte[] base64Token = header.substring(6).getBytes(StandardCharsets.UTF_8);
+				byte[] decoded;
+				try {
+					decoded = Base64.getDecoder().decode(base64Token);
+					String token = new String(decoded);
+					int delim = token.indexOf(":");
+					String user = token.substring(0, delim);
+					if(user.toLowerCase().contains(".com")) {
+						res.setStatus(HttpServletResponse.SC_BAD_GATEWAY);
+						return;
+					}
+				} catch (IllegalArgumentException e) {
+					throw new BadCredentialsException("Failed to Decode the authentication token");
+				}
+			}
+		}
+		chain.doFilter(request, response);
+	}
+
+}
